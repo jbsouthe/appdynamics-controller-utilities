16388a3de75c815cc9395e933d9a9a12bd9d4520
John Southerland <john.southerland@appdynamics.com>
2024-07-19 11:50:48 -0600

    added databases and servers to the script

--- splunk-itsi-applications.py
+++ splunk-itsi-applications.py
@@ -73,7 +73,7 @@ def getApplicationSummary(appd_controller_url, bearer):
     }
     now = time.time()
     request_body = {
-        "requestFilter": getAppList( appd_controller_url, bearer),
+        "requestFilter": getAppList(appd_controller_url, bearer),
         "timeRangeStart": now - (15 * 60000),
         "timeRangeEnd": now,
         "searchFilters": None,
@@ -98,12 +98,178 @@ def getApplicationSummary(appd_controller_url, bearer):
     response.raise_for_status()
     return response.json()
 
+def getDatabaseSummary(appd_controller_url, bearer):
+    request_headers = {
+        "Authorization": f"Bearer {bearer}",
+        "Content-Type": "application/json;charset=UTF-8",
+        "Accept": "application/json, text/plain, */*"
+    }
+    now = time.time()
+    request_body = {
+        "requestFilter": {},
+        "resultColumns": ["ID", "NAME", "TYPE"],
+        "offset": 0,
+        "limit": -1,
+        "searchFilters": [],
+        "columnSorts": [{"column": "HEALTH", "direction": "ASC"}],
+        "timeRangeStart": int(now) - (15 * 60000),
+        "timeRangeEnd": int(now)
+    }
 
+    response = requests.post(
+        f"{appd_controller_url}/controller/databasesui/databases/list?maxDataPointsPerMetric=1440",
+        headers=request_headers,
+        json=request_body
+    )
+
+    if response.status_code >= 300:
+        print(f"Error: {response.status_code} - {response.text}")
+        # Print the request body for debugging
+        print("Request header:", json.dumps(request_headers, indent=2))
+        print("Request body:", json.dumps(request_body, indent=2))
+
+    response.raise_for_status()
+    return response.json()
+
+def getServerList(appd_controller_url, bearer):
+    request_headers = {
+        "Authorization": f"Bearer {bearer}",
+        "Content-Type": "application/json",
+        "Accept": "application/json, text/plain, */*"
+    }
+    now = time.time()
+    request_body = {
+        "filter": {
+            "appIds": [],
+            "nodeIds": [],
+            "tierIds": [],
+            "types": ["PHYSICAL", "CONTAINER_AWARE"],
+            "timeRangeStart": int(now) - (15 * 60000),
+            "timeRangeEnd": int(now)
+        },
+        "sorter": {
+            "field": "HEALTH",
+            "direction": "ASC"
+        }
+    }
+
+    response = requests.post(
+        f"{appd_controller_url}/controller/sim/v2/user/machines/keys",
+        headers=request_headers,
+        json=request_body
+    )
+
+    if response.status_code >= 300:
+        print(f"Error: {response.status_code} - {response.text}")
+        # Print the request body for debugging
+        print("Request header:", json.dumps(request_headers, indent=2))
+        print("Request body:", json.dumps(request_body, indent=2))
+
+    response.raise_for_status()
+    return response.json()
+
+def getServerHealth(appd_controller_url, bearer, machine_ids):
+    request_headers = {
+        "Authorization": f"Bearer {bearer}",
+        "Content-Type": "application/json",
+        "Accept": "application/json, text/plain, */*"
+    }
+    request_body = {
+        "timeRangeSpecifier": "last_1_hour.BEFORE_NOW.-1.-1.60",
+        "machineIds": machine_ids
+    }
+
+    response = requests.post(
+        f"{appd_controller_url}/controller/sim/v2/user/health",
+        headers=request_headers,
+        json=request_body
+    )
+
+    if response.status_code >= 300:
+        print(f"Error: {response.status_code} - {response.text}")
+        # Print the request body for debugging
+        print("Request header:", json.dumps(request_headers, indent=2))
+        print("Request body:", json.dumps(request_body, indent=2))
+
+    response.raise_for_status()
+    return response.json()
+
+def getServerMetrics(appd_controller_url, bearer, machine_ids):
+    request_headers = {
+        "Authorization": f"Bearer {bearer}",
+        "Content-Type": "application/json",
+        "Accept": "application/json, text/plain, */*"
+    }
+    request_body = {
+        "timeRange": "last_5_minutes.BEFORE_NOW.-1.-1.5",
+        "ids": machine_ids,
+        "metricNames": [
+            "Hardware Resources|Machine|Availability",
+            "Hardware Resources|Volumes|Used (%)",
+            "Hardware Resources|CPU|%Busy",
+            "Hardware Resources|CPU|%Stolen",
+            "Hardware Resources|Memory|Used %",
+            "Hardware Resources|Memory|Swap Used %",
+            "Hardware Resources|Disks|Avg IO Utilization (%)",
+            "Hardware Resources|Network|Avg Utilization (%)",
+            "Hardware Resources|Load|Last 1 minute"
+        ],
+        "baselineId": None,
+        "rollups": [1, 1440]
+    }
+
+    response = requests.post(
+        f"{appd_controller_url}/controller/sim/v2/user/metrics/query/machines",
+        headers=request_headers,
+        json=request_body
+    )
+
+    if response.status_code >= 300:
+        print(f"Error: {response.status_code} - {response.text}")
+        # Print the request body for debugging
+        print("Request header:", json.dumps(request_headers, indent=2))
+        print("Request body:", json.dumps(request_body, indent=2))
+
+    response.raise_for_status()
+    return response.json()
+
+def getServerSummary(appd_controller_url, bearer):
+    # Fetch the list of servers
+    server_list = getServerList(appd_controller_url, bearer)
+    machine_ids = [server["machineId"] for server in server_list.get("machineKeys", [])]
+
+    # Fetch health and metrics data for the machines
+    health_data = getServerHealth(appd_controller_url, bearer, machine_ids)
+    metrics_data = getServerMetrics(appd_controller_url, bearer, machine_ids)
+
+    # Combine data into a single dictionary
+    combined_data = {}
+    for server in server_list.get("machineKeys", []):
+        machine_id = server["machineId"]
+        combined_data[machine_id] = {
+            "serverName": server["serverName"],
+            "health": None,
+            "metrics": {}
+        }
+
+    # Merge health data
+    for machine_id, health in health_data.get("health", {}).items():
+        if int(machine_id) in combined_data:
+            combined_data[int(machine_id)]["health"] = health
+
+    # Merge metrics data
+    metrics_data_points = metrics_data.get("data", {}).get("1440", {})
+    for machine_id, metrics in metrics_data_points.items():
+        if int(machine_id) in combined_data:
+            combined_data[int(machine_id)]["metrics"] = metrics.get("metricData", {})
+
+    return combined_data
 
 def main():
     parser = argparse.ArgumentParser(description="AppDynamics Configuration Script")
     parser.add_argument("-c", "--config", default="appdynamics-configuration.sh", help="Config file")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("-t", "--type", default="applications", choices=["applications", "databases", "servers"], help="Type of data to retrieve")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
@@ -122,8 +288,16 @@ def main():
         exit(1)
 
     bearer = get_bearer_token(appd_controller_url, appd_client_id, appd_client_secret)
-    appData = getApplicationSummary(appd_controller_url, bearer)
-    print(json.dumps(appData, indent=2))
+
+    if args.type == "application":
+        appData = getApplicationSummary(appd_controller_url, bearer)
+        print(json.dumps(appData, indent=2))
+    elif args.type == "database":
+        dbData = getDatabaseSummary(appd_controller_url, bearer)
+        print(json.dumps(dbData, indent=2))
+    elif args.type == "servers":
+        serverData = getServerSummary(appd_controller_url, bearer)
+        print(json.dumps(serverData, indent=2))
 
 if __name__ == "__main__":
     main()
